cb1997f35c43845aa9e5d2b3a6e7a69ae1deac37
Mark Zietara <zietaramark@gmail.com>
2021-01-10 19:19:24 -0500

    gitignore and simplifying init script with permissions to user for dotfiles

--- init.sh
+++ init.sh
@@ -7,7 +7,9 @@ apt update
 apt-get update
 apt-get upgrade -y
 
-apt install -y vim
+apt install -y vim \
+    ripgrep \
+    fzf
 
 apt-get install -y \
     git \
@@ -15,35 +17,16 @@ apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-
-## instal brew for homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-## Install ag for searching quickly
-#echo "installing ripgrep, a quick searcher"
-#brew install ripgrep
-
-## install unzip
-apt-get install -y unzip
-
-## ctags
-apt-get install -y ctags
-
-## yamllint
-apt-get install -y yamllint
-
-## Install go
-apt install -y golang
-
-apt-get install -y zsh
+    unzip \
+    ctags \
+    fonts-powerline \
+    yamllint \
+    golang
 
 curl -o /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v4.3.1/googler
 chmod +x /usr/local/bin/googler
 googler -u
 
-mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/trapd00r/LS_COLORS/tarball/master | tar xzf  - --directory=/tmp/LS_COLORS --strip=1
-( cd /tmp/LS_COLORS && sh install.sh  )
-
-bash init-dotfiles.sh $GITHUB_USER $USERNAME
-
-
+sudo -u $USERNAME bash init-dotfiles.sh $GITHUB_USER $USERNAME
+## install brew for homebrew
+#sudo -u $USERNAME /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
